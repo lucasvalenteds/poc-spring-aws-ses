9fd8ee1579c8263387dbb63bbb0bc5e7f1720fbb
Lucas <lucasvalenteds@gmail.com>
2021-10-03 16:33:48 -0300

    Creating Java project

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.logging.log4j", "log4j-api", properties["version.log4j"].toString())
+    implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
+    implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
+
+    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+    }
+}

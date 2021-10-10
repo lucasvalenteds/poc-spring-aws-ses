d74c61341921448644c0225468868d651f4f3635
Lucas <lucasvalenteds@gmail.com>
2021-10-09 23:26:49 -0300

    Converting POJOs to Records

--- src/main/java/com/example/user/UserCreatedProperties.java
+++ src/main/java/com/example/user/UserCreatedProperties.java
@@ -2,37 +2,5 @@ package com.example.user;
 
 import com.example.email.EmailProperties;
 
-import java.util.Objects;
-
-public final class UserCreatedProperties implements EmailProperties {
-
-    private final User user;
-
-    public UserCreatedProperties(User user) {
-        this.user = user;
-    }
-
-    public User user() {
-        return user;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (UserCreatedProperties) obj;
-        return Objects.equals(this.user, that.user);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(user);
-    }
-
-    @Override
-    public String toString() {
-        return "UserCreatedProperties[" +
-            "user=" + user + ']';
-    }
-
+public record UserCreatedProperties(User user) implements EmailProperties {
 }
--- src/main/java/com/example/user/UserDeletedProperties.java
+++ src/main/java/com/example/user/UserDeletedProperties.java
@@ -2,45 +2,5 @@ package com.example.user;
 
 import com.example.email.EmailProperties;
 
-import java.util.Objects;
-
-public final class UserDeletedProperties implements EmailProperties {
-
-    private final String email;
-    private final String firstname;
-
-    public UserDeletedProperties(String email, String firstname) {
-        this.email = email;
-        this.firstname = firstname;
-    }
-
-    public String email() {
-        return email;
-    }
-
-    public String firstname() {
-        return firstname;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (UserDeletedProperties) obj;
-        return Objects.equals(this.email, that.email) &&
-            Objects.equals(this.firstname, that.firstname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(email, firstname);
-    }
-
-    @Override
-    public String toString() {
-        return "UserDeletedProperties[" +
-            "email=" + email + ", " +
-            "firstname=" + firstname + ']';
-    }
-
+public record UserDeletedProperties(String email, String firstname) implements EmailProperties {
 }
